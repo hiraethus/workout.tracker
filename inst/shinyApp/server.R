efa20cc50259077790c1b76ae0c73128f426db3e
Mike Jones <mjclackjones@gmail.com>
2016-01-01 20:32:36 +0000

    Show stats for current bmi and number of exercises this week in Shiny app

--- inst/shinyApp/server.R
+++ inst/shinyApp/server.R
@@ -1,5 +1,23 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  output$workouts.table <- shiny::renderTable(workout.tracker::generate.report(input$xml.document$datapath)$data)
+  result <- reactive({
+    if (!is.null(input$xml.document)) {
+      workout.tracker::generate.report(input$xml.document$datapath)
+    } else {
+      NULL
+    }
+  })
+  
+  output$workouts.table <- renderTable({
+    if (!is.null(result())) result()$data
+  })
+  
+  output$current.bmi <- renderText({
+    if (!is.null(result())) tail(result()$data$bmi, n=1)
+  })
+  
+  output$workouts.this.week <- renderText({
+    if (!is.null(result())) tail(result()$week.workouts$talley, n=1)
+  })
 })
--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -8,11 +8,16 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput("xml.document",
-                  "Workout xml doc",
+                  "Workout data (xml)",
                   multiple=F,
     )),
     
     mainPanel(
+      h3('This week stats'),
+      p('BMI:  '),
+      p(textOutput("current.bmi")),
+      p('Num workouts:  '),
+      p(textOutput("workouts.this.week")),
       tableOutput("workouts.table")
     )
   )
