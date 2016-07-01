d52e2db234ffdc20fcbfd368407dbc799a2e4a61
Michael Jones <mjclackjones@gmail.com>
2016-07-01 16:34:32 +0100

    Add columns and rows to graphs

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -8,27 +8,50 @@ shinyUI(fluidPage(
   sidebarLayout(
     mainPanel(
       h3('3 Month summary'),
-      h4('Workout Frequency'),
-      plotOutput("workout.frequency.plot"),
-      h4('Distance Covered'),
-      plotOutput('distance.covered.plot'),
-      h4('Weight'),
-      plotOutput('weight.plot'),
-      h4('Waist size'),
-      plotOutput('waist.size.plot'),
-      h3('Workout data'),
-      tableOutput("workouts.table")
+      fluidRow(
+        column(6,
+             h4('Workout Frequency'),
+             plotOutput("workout.frequency.plot")
+        ),
+        column(6,
+               h4('Distance Covered'),
+               plotOutput('distance.covered.plot')
+        )
+      ),
+      fluidRow(
+        column(6,
+               h4('Weight'),
+               plotOutput('weight.plot')
+        ),
+        column(6,
+               h4('Waist size'),
+               plotOutput('waist.size.plot')
+        )
+        
+      ),
+      fluidRow(
+        column(6,
+                h3('Workout data'),
+               tableOutput("workouts.table")
+        )
+      )
     ),
     
     sidebarPanel(
       fileInput("xml.document",
                 "Workout data (xml)",
-                multiple=F,
+                multiple=F
       ),
       h3('This week stats'),
-      h4('BMI:  '),
-      p(textOutput("current.bmi")),
-      h4('Num workouts:  '),
-      p(textOutput("workouts.this.week")))
+      fluidRow(
+        column(3, h4('BMI:  ')),
+        column(2, p(textOutput("current.bmi")))
+      ),
+      fluidRow(
+        column(3,h4('Num workouts:  ')),
+        column(2,p(textOutput("workouts.this.week")))
+      )
+      
+    )
   )
 ))
