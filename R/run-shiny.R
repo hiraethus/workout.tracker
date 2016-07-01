e54a2495c5282eee94ea30c6a72f8c9829914f99
Michael Jones <mjclackjones@gmail.com>
2016-07-01 14:46:33 +0100

    set shiny app to always use port 9999

--- R/run-shiny.R
+++ R/run-shiny.R
@@ -4,5 +4,5 @@ run.server <- function() {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
   }
   
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(appDir, display.mode = "normal", port = 9999)
 }
